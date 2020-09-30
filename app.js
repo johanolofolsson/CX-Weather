4263d2d3ded0b4e2d7ff4eb75350c525407b60ea
folo1988 <johan@olssons.org>
2020-09-30 22:11:51 +0100

    Tidy up of `app.js` and maps out routes that need to be built later.

--- app.js
+++ app.js
@@ -2,31 +2,38 @@
 const express = require('express'); // Server
 const app = express();
 
-require('dotenv').config();
 
+require('dotenv').config(); // For secrets
 const crypto = require('crypto'); // For HMAC support
-
 app.use(express.json()); // To parse JSON body
+
+
+const installRoutes     = require('./routes/install');
+// const configRoutes    = require('./routes/config');
+// const statusRoutes    = require('./routes/status');
+// const uninstallRoutes = require('./routes/uninstall');
+
+// const serviceConfigRoutes  = require('./routes/service/config');
+// const serviceStatusRoutes  = require('./routes/service/status');
+// const serviceInvokeRoutes  = require('./routes/service/invoke');
+// const serviceCreateRoutes  = require('./routes/service/create');
+// const serviceDeleteRoutes  = require('./routes/service/delete');
+// const serviceWeatherRoutes = require('./routes/service/weather');
 
 
 
 // Routes
-const installRoutes = require('./routes/install');
-const weatherRoutes = require('./routes/weather');
-
-
-
-// app.use('/status', statusRoutes);
-
-app.use('/install', installRoutes);
-
+app.use('/install',     installRoutes);
+// app.use('/config',    configRoutes);
+// app.use('/status',    statusRoutes);
 // app.use('/uninstall', uninstallRoutes);
 
-// app.use('/configure', configureRoutes);
-
-// app.use('/save', saveRoutes);
-
-app.use('/weather', weatherRoutes);
+// app.use('service/config',  serviceConfigRoutes);
+// app.use('service/status',  serviceStatusRoutes);
+// app.use('service/invoke',  serviceInvokeRoutes);
+// app.use('service/create',  serviceCreateRoutes);
+// app.use('service/delete',  serviceDeleteRoutes);
+// app.use('service/weather', serviceWeatherRoutes);
 
 
 
