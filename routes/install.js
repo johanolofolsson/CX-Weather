c73cbbdc3388dd89e67bac9791caab7cd1066da5
folo1988 <johan@olssons.org>
2020-10-05 14:27:02 +0100

    Adds JWT verification, and final version of `fs.writeFile` proof-of-concept (will be replaced with Firestore).

--- routes/install.js
+++ routes/install.js
@@ -13,14 +13,24 @@ const installsData = fs.readFileSync('./data/installs.json');
 
 
 
-// TODO: https://www.youtube.com/watch?v=mbsmsi7l3r4&ab_channel=WebDevSimplified
-const JWTpayload = '';
+router.post('/', auth, (req, res) => {
+  const installBody = req.body;
+  const installUUID = installBody.applicationInstall.uuid;
+  const installTime = req.payload.iat;
+  const productSub  = req.payload.sub;
+  const tenantID    = req.payload["o.a.p.ctenantId"];
 
-router.post('/', (req, res) => {
-  // const JWTsecret = req.body.secret;
-  // console.log(JWTsecret);
-  const installBody = JSON.stringify(req.body, null, 2);
-  fs.writeFile('./data/installs.json', installBody, (err) => {
+  const installDetails = {
+    installID: installUUID,
+    installTime: installTime,
+    productSub: productSub,
+    tenantID: tenantID
+  };
+
+  const installDetailsJSON = JSON.stringify(installDetails, null, 2);
+
+
+  fs.writeFile('./data/installs.json', installDetailsJSON, (err) => {
     if (err) {
 
       res.status(500).json({
@@ -31,18 +41,35 @@ router.post('/', (req, res) => {
 
       res.status(200).json({
         message: 'Install details saved',
-        // auth: req.headers.authorization,
-        sig: req.body.secret
+        installUUID: installUUID
       });
 
     };
 
   });
-  // TODO: check headers
   
-  // TODO: log body / later parse and assign to vars
-  // TODO: write to a JSON file, record 'tenants' who install the app
 });
+
+
+
+// https://www.youtube.com/watch?v=mbsmsi7l3r4&ab_channel=WebDevSimplified
+// https://github.com/WebDevSimplified/JWT-Authentication/blob/master/server.js
+function auth(req, res, next) {
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+  
+  console.log(authHeader);
+  console.log(token);
+
+  if (token == null) return res.sendStatus(401)
+
+  jwt.verify(token, process.env.AMS_SECRET, (err, payload) => {
+    console.log(err)
+    if (err) return res.sendStatus(403)
+    req.payload = payload
+    next()
+  })
+};
 
 
 
